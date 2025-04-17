36d186280c9a49445c7e21e0d9851a0c38f4830b
Potato228 <petruhakabachok@gmail.com>
2025-04-17 14:34:14 +0800

    .gitignore updated, YandexMetrika added, privacy policy page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import "./globals.css";
 import localFont from "next/font/local";
 import Script from "next/script";
 import { fetchSEOData } from "@/lib/fetchSEOData";
+import { Suspense } from "react";
+import YandexMetrika from "@/components/YandexMetrika/YandexMetrika";
 
-export async function generateMetadata(){
-    const metadata = await fetchSEOData('home')
-    return metadata
-};
+export async function generateMetadata() {
+	const metadata = await fetchSEOData("home");
+	return metadata;
+}
 
 const nk133 = localFont({
 	src: "../assets/fonts/NK133/nk133.otf",
@@ -60,8 +62,45 @@ export default function RootLayout({
                             `,
 						}}
 					/>
+
+					<meta name="yandex-verification" content="825d8321fb9234bd" />
 				</head>
 				<body>
+					{/* <!-- Yandex.Metrika counter --> */}
+					<Script strategy="afterInteractive">
+						{
+							`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+							m[i].l=1*new Date();
+							for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+							k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+							(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+							ym(100763687, "init", {
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true,
+								webvisor:true
+							});`
+						}
+					</Script>
+
+					<noscript>
+						<div>
+							<img
+								src="https://mc.yandex.ru/watch/100763687"
+								style={{
+									position: "absolute",
+									left: "-9999px",
+								}}
+								alt=""
+							/>
+						</div>
+					</noscript>
+					{/* <!-- /Yandex.Metrika counter --> */}
+					
+					<Suspense fallback={<></>}>
+						<YandexMetrika />
+					</Suspense>
 					<div className="mainContainer">
 						<main className="main">{children}</main>
 					</div>
